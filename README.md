1e6d34dcd40d15d44f6864b87468c85b4751fa56
FelipeAlves07 <fellipeallves07@gmail.com>
2022-04-16 12:43:49 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+Projeto de um site de uma papelaria personaliza.
+
+Link do projeto: 
---
+++ formulario.php
@@ -0,0 +1,44 @@
+<!-- Não funcionou -->
+
+<?php
+
+
+/*if(isset($_POST(['email'])) && !empty($_POST['email']){
+
+$Serviço_solicitado = addslashes($_POST['Serviço solicitado'])
+$Temada_festa = addslashes($_POST['Tema da festa'])
+$Nome_do_aniversariante_afins = addslashes($_POST['Nome do aniversariante/afins'])
+$Idade_aniversariante_afins = addslashes($_POST['Idade solicitada'])
+$Local_e_dia = addslashes($_POST['Local e dia'])
+$Estilo_da_festa = addslashes($_POST['Estilo da festa'])
+$Orçamento_base = addslashes($_POST['Orçamento base'])
+$whatsapp = addslashes($_POST['whatsapp'])
+$email = addslashes($_POST(['email']))
+$acrescentação = addslashes($_POST['acrescento'])
+
+$to = "paperdecorpp@gmail.com";
+$subject = "Orçamento Paper-Decor";
+$body = "Serviço Solicitado: ".$Serviço_solicitado. "\n".
+        "Tema da Festa: ".$Temada_festa. "\n".
+        "Nome do aniversariante/afins: ".$Nome_do_aniversariante_afins. "\n.
+        "Idade solicitada: ".$Idade_aniversariante_afins. "\n".
+        "Local e dia: ".$Local_e_dia. "\n".
+        "Estilo da festa: ".$Estilo_da_festa. "\n".
+        "Orçamento base: ".$Orçamento_base. "\n".
+        "whatsapp: ".$whatsapp. "\n".
+        "email: ".$email. "\n".
+        "acrescentação: ".$acrescentação;
+
+$header = "From:"."\r\n".
+            "Reply-To:".$email."\r\n".
+            "X=Mailer:PHP/".phpversion();
+mail($to,$subject,$body,$header)){
+
+  echo("Pedido de orçamento concluído com sucesso!");
+}else{
+  echo("O pedido não pôde ser concluído! Tente novamente.");
+}
+
+}
+
+?> */
